95dd39391fe3cf9b951e9e8abd27931857b5f37f
Cássio Gadêlha <61876421+cassiogadelha@users.noreply.github.com>
2025-02-27 08:51:30 -0300

    I dont know

--- Game/Scenes/Enemy/skeleton_warrior.gd
+++ Game/Scenes/Enemy/skeleton_warrior.gd
@@ -1,14 +1,14 @@
 extends Enemy
 
 func _physics_process(delta: float) -> void:
-    move_to_player(delta)
+	move_to_player(delta)
 
 
 func _on_attack_timer_timeout() -> void:
-    $Timers/AttackTimer.wait_time = rng.randf_range(2.5, 3.5)
+	$Timers/AttackTimer.wait_time = rng.randf_range(2.5, 3.5)
 
-    if position.distance_to(player.position) < attack_radius:
-        $AnimationTree.set("parameters/OneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+	if position.distance_to(player.position) < attack_radius:
+		$AnimationTree.set("parameters/OneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
 
 func can_damage(value: bool) -> void:
-    $Skin/Rig/Skeleton3D/BoneAttachment3D/Bone.can_damage = value
+	$Skin/Rig/Skeleton3D/BoneAttachment3D/Bone.can_damage = value
--- Game/Scenes/Level/level.gd
+++ Game/Scenes/Level/level.gd
@@ -4,22 +4,22 @@ extends Node3D
 var fireball_scene: PackedScene = preload("res://Scenes/VFX/fire_ball.tscn")
 
 const scenes: Dictionary = {
-    'dungeon': "res://Scenes/Level/dungeon.tscn",
-    'overworld': "res://Scenes/Level/overworld.tscn",
+	'dungeon': "res://Scenes/Level/dungeon.tscn",
+	'overworld': "res://Scenes/Level/overworld.tscn",
 }
 
 func _ready() -> void:
-    for entity: Node3D in $Entities.get_children():
-        if entity.has_signal("cast_spell"):
-            entity.connect("cast_spell", create_fireball)
+	for entity: Node3D in $Entities.get_children():
+		if entity.has_signal("cast_spell"):
+			entity.connect("cast_spell", create_fireball)
 
 func create_fireball(_type:String, _pos:Vector3, _direction:Vector2, _size:float) -> void:
-    var fireball: Fireball = fireball_scene.instantiate()
-    $Projectiles.add_child(fireball)
+	var fireball: Fireball = fireball_scene.instantiate()
+	$Projectiles.add_child(fireball)
 
-    fireball.global_position = _pos
-    fireball.direction = _direction
-    fireball.setup(_size)
-    
+	fireball.global_position = _pos
+	fireball.direction = _direction
+	fireball.setup(_size)
+	
 func switch_level(level_name: String) -> void:
-    get_tree().change_scene_to_file(scenes[level_name])
+	get_tree().change_scene_to_file(scenes[level_name])
